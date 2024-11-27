650a4fe298a602ab9ec2bec78edeaaabe7e81c31
seoyoungkyung <dnsdka202@naver.com>
2024-11-27 15:05:06 +0900

    two2

--- src/main/java/project/invitation/controller/HomeController.java
+++ src/main/java/project/invitation/controller/HomeController.java
@@ -1,4 +1,4 @@
-package invitation.project.controller;
+package project.invitation.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
--- src/main/java/project/invitation/controller/MessageController.java
+++ src/main/java/project/invitation/controller/MessageController.java
@@ -1,4 +1,4 @@
-package invitation.project.controller;
+package project.invitation.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
