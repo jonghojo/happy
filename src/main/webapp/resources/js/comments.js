f8b69bfa1c203eb95f8a91ff2fe8c71a2f244db7
seoyoungkyung <dnsdka202@naver.com>
2024-12-02 15:00:19 +0900

    Update comments.js

--- src/main/webapp/resources/js/comments.js
+++ src/main/webapp/resources/js/comments.js
@@ -1,61 +1,72 @@
+console.log("Reply Module.....");
 
 let commentsService = (function(){
     
-    function add(comments, callback, error){
-        console.log("comments.....");
-
-        $.ajax({
-            type: 'post',
-            url: '/comments/new',
-            data: JSON.stringify(comments),
-            contentType: "application/json; charset=utf-8",
-            success: function(result, status, xhr){
-                if(callback){
-                    callback(result);
-                }
-            },
-            error: function(xhr, status, er){
-                if(error){
-                    error(er);
-                }
-            }
-        })
-    } // add end
+    function add(comments, callback, error) {
+	
+	    let commenter = comments.commenter || "Anonymous"; // 기본값 설정
+	
+	    comments.commenter = commenter; // commenter 설정
+	    
+	    $.ajax({
+	        type: 'post',
+	        url: '/comments/new',
+	        data: JSON.stringify(comments),
+	        contentType: "application/json; charset=utf-8",
+	        success: function(result, status, xhr){
+	            if(callback){
+	                callback(result);
+	            }
+	        },
+	        error: function(xhr, status, er){
+	            if(error){
+	                error(er);
+	            }
+	        }
+	    });
+	}
 
     
 //getList start
-    function getList(mno, callback, error){
-        console.log(mno);
+    function getList(param, callback, error){
+    
+    	let mno = param.mno;
+    	
+    
+        console.log("mno:", mno);  // mno 값 확인
+    	
 
-        $.ajax({
-            type: 'get',
-            url: "/comments/" + mno,  // 
-            success: function(data, status, xhr){
-                console.log("data");
-                if(callback){
-                    callback(data);  // 전체 댓글 리스트만 반환
-                }
-            },
-            error: function(xhr, status, er){
-                if(error){
-                    error(er);
-                }
-            }
-        });
+         $.ajax({
+	        type: 'get',
+	        url: "/comments/list/" + mno,  // 페이지 번호를 URL에 포함
+	        success: function(data, status, xhr) {
+			    console.log("data:", data);
+				    if (Array.isArray(data)) {
+				        callback(data);  // 데이터가 배열인 경우
+				    } else if (data.list) {
+				        callback(data.list);  // list 속성이 있는 객체인 경우
+				    } else {
+				        console.error("Unexpected data structure:", data);
+				    }
+			},
+	        error: function(xhr, status, er){
+	            if(error){
+	                error(er);
+	            }
+	        }
+	    });
     }
     // getList end
 
     //remove start
-    function remove(cno, writer, callback, error){   
+    function remove(cno, commenter, callback, error){   
         $.ajax({
             type: 'delete',
             url: '/comments/' + cno,
-            data: JSON.stringify({cno:cno, writer:writer}),
+            data: JSON.stringify({cno : cno, commenter: commenter}),
             contentType: "application/json; charset=utf-8",
-            success: function(deleteReuslt, status, xhr){
-                if(callback){
-                    callback(deleteReuslt);
-                }
+            success: function(response){
+                if(callback) callback(response);
             },
             error: function(xhr, status,er){
                if (error){
@@ -68,6 +79,9 @@ let commentsService = (function(){
 
     //update start
     function update(comments, callback, error){
+	    if (!comments.commenter) {
+	        return error("댓글 작성자가 없습니다.");
+	    }
         $.ajax({
             type: 'put',
             url: "/comments/" + comments.cno,
@@ -91,7 +105,7 @@ let commentsService = (function(){
     function get(cno, callback, error){
         $.ajax({
             type: 'get',
-            url: "/comments/" + cno,
+            url: "/comments/detail/" + cno,
             success: function(commentsVO, status, xhr){
                 if(callback){
                     callback(commentsVO);
@@ -105,4 +119,12 @@ let commentsService = (function(){
         });
     }
     //get end
-});
+    
+     return {
+        add: add,
+        getList: getList,
+        remove: remove,
+        update: update,
+        get: get
+    };
+})();
